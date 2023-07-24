6f9d3acb97a1696d4ef367471c168b1445976685
Donald Hunter <donaldhunter262@gmail.com>
2023-07-23 17:34:07 -0700

    5:34pm

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -25,7 +25,8 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Tab One',
+          title: 'Start Workout',
+          headerShown: false,
           tabBarIcon: ({ color }) => <TabBarIcon name="code" color={color} />,
           headerRight: () => (
             <Link href="/modal" asChild>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,23 +1,62 @@
-import { StyleSheet } from 'react-native';
-
+import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import { DarkTheme, DefaultTheme, ThemeProvider, useTheme } from '@react-navigation/native';
+import { useColorScheme } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as Font from 'expo-font';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
+import { ScrollView } from 'react-native-gesture-handler';
+
+function Icon(props: {
+  name: React.ComponentProps<typeof FontAwesome>['name'];
+  color: string;
+  size: number; // Add the size property as a number
+}) {
+  const { name, color, size } = props;
+
+  return <FontAwesome name={name} size={size} color={color} style={{ marginBottom: 0 }} />;
+}
+
+const fetchFonts = async () =>
+  await Font.loadAsync({
+    'Virgil': require('../../assets/fonts/Virgil 3 YOFF.ttf'),
+  });
 
 export default function TabOneScreen() {
+  const theme = useTheme(); // Get the current theme from ThemeProvider
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
-    </View>
+    <SafeAreaView style={[styles.container, { backgroundColor: theme.colors.background }]}>
+
+      <View style={{ flexDirection: 'row', backgroundColor: theme.colors.background, justifyContent:'space-between', alignItems:'center'}}>
+        <View style={{backgroundColor: theme.colors.background}}>
+          <Text style={[styles.title, { marginLeft: 0 }]}>7/23/23</Text>
+          <Text style={[styles.title, { fontSize: 40 }]}>Sunday</Text>
+        </View>
+        <Icon name="user-circle" color="black" size={40}/>
+      </View>
+
+  
+      <View style={[styles.mainButton, {backgroundColor: theme.colors.primary}]}>
+        <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 25 }}>Start Workout</Text>
+      </View>
+
+      <Text style={{ marginTop: 90, fontWeight: '500', fontSize: 28 }}>Recent Workouts</Text>
+
+      <Text style={{fontFamily:'Virgil'}}>xcxxzcvxcvxcvxcvbashi</Text>
+
+
+      {/* ... */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: '5%',
+    marginTop: 60,
   },
   title: {
     fontSize: 20,
@@ -27,5 +66,13 @@ const styles = StyleSheet.create({
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  mainButton: {
+    marginTop: 30,
+    height: 60,
+    width: '100%',
+    borderRadius: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
